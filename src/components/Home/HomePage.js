68453c2e272ebd7a830f11ab1935f891fc37609f
chrisHebs23 <christopher.heberden@gmail.com>
2021-03-12 15:51:59 -0800

    fix bugs and some features

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -12,16 +12,15 @@ export default class HomePage extends Component {
           <style>{"body { background-color: #0b0c10; }"}</style>
         </Helmet>
         <h1 className="title layout-align">Chris Heberden</h1>
-        <h2 className="text layout-align">
-          <TextLoop
-            springConfig={{ stiffness: 180, damping: 8 }}
-            interval={2000}
-          >
-            <span>Web Developer</span>
-            <span>Sportsman and Sports Lover</span>
-            <span>Eco Enthusiast</span>
-          </TextLoop>
-        </h2>
+        <TextLoop
+          springConfig={{ stiffness: 180, damping: 8 }}
+          interval={3000}
+          className="text-loop layout-align"
+        >
+          <span className="layout-align">Web Developer</span>
+          <span className="layout-align">Sportsman and Sports Lover</span>
+          <span className="layout-align">Eco Enthusiast</span>
+        </TextLoop>
 
         <Link to="/contact" className=" page-button layout-align">
           <button className=" buttons layout-align ">Get In Touch </button>
